abfae58242a164cacd9594cc02e11860929788b7
buitruyenpro <namtruyenpro95@gmail.com>
2020-07-01 17:17:20 +0700

    ok

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using API.Errors;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -5,6 +7,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,12 +35,32 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            // Đây là khi đầu vào input không hợp lệ
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Đây là khi lỗi server
             app.UseMiddleware<ExceptionMiddleware>();
+            // Đây là trang không tìm thấy
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
