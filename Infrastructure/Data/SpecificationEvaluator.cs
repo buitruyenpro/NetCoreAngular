ea331ccf633d68694bfd75b541e54a46ca398ef9
buitruyenpro <namtruyenpro95@gmail.com>
2020-07-01 12:28:15 +0700

    ok

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -7,6 +7,7 @@ namespace Infrastructure.Data
 {
     public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
     {
+        // Mục đích trả về chuỗi truy vấn mà thôi
         public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
         {
             var query = inputQuery;
