b99c9dedb688b0843109e6ed49eb8a217aab2ef5
buitruyenpro <namtruyenpro95@gmail.com>
2020-07-01 09:45:32 +0700

    ok

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -27,6 +28,8 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+            var typeId = 1;
+            var producs = _context.Products.Where(x => x.ProductTypeId == typeId).Include(x => x.ProductType).TolistAsync();
             return await _context.Products
             .Include(p => p.ProductType)
             .Include(p => p.ProductBrand)
