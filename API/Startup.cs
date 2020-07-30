9fbbc4c371b14b18b6c361dbb5a2ded5066750ef
buitruyenpro <namtruyenpro95@gmail.com>
2020-07-30 15:46:06 +0700

    ok

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,13 @@ namespace API
             // Đây là khi đầu vào input không hợp lệ
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -49,7 +56,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwaggerDocumention();
             app.UseEndpoints(endpoints =>
