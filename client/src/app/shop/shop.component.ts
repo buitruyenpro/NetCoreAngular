b269033ff3df2d24e7f71af86f94d2ad8b21c658
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-01 14:13:35 +0700

    ok

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -15,6 +15,12 @@ export class ShopComponent implements OnInit {
   types: IType[];
   brandIdSelected = 0;
   typeIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to High', value: 'priceAsc' },
+    { name: 'Price: High to Low', value: 'priceDesc' },
+  ];
 
   constructor(private shopService: ShopService) {}
 
@@ -25,7 +31,7 @@ export class ShopComponent implements OnInit {
   }
   getProducts() {
     this.shopService
-      .getProducts(this.brandIdSelected, this.typeIdSelected)
+      .getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected)
       .subscribe(
         (respone) => {
           this.products = respone.data;
@@ -62,6 +68,10 @@ export class ShopComponent implements OnInit {
 
   onTypeSelected(typeId: number) {
     this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+  onSortSelected(sort: string) {
+    this.sortSelected = sort;
     this.getProducts();
   }
 }
