290bb58b9f6eac88d9103edb91c5ea833d6fcbd9
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-01 14:34:05 +0700

    ok

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ShopComponent } from './shop.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +6,7 @@ import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
