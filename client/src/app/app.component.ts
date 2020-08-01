dc622d134e4da1703843d21cccaa87c590e37b66
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-01 11:02:43 +0700

    ok

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,11 +10,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  products = [];
+  products: IProduct[];
+
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: any) => {
+      (response: IPagination) => {
         this.products = response.data;
       },
       (error) => {
