5962eb46ce0eadbc628ac37608476d2435f295fa
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-02 10:40:00 +0700

    ok

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 
@@ -19,6 +20,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
