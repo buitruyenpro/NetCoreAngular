863d9aa4dcdd1d7b8fe1c53d3c0ffc6a04dd7229
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-02 19:42:00 +0700

    ok

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,11 +12,13 @@ import { BreadcrumbService } from 'xng-breadcrumb';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(
     private shopService: ShopService,
     private activateRoute: ActivatedRoute,
-    private bcService: BreadcrumbService
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
   ) {
     this.bcService.set('@productDetails', '');
   }
@@ -24,17 +27,29 @@ export class ProductDetailsComponent implements OnInit {
     this.loadProduct();
   }
 
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
   loadProduct() {
-    this.shopService
-      .getProduct(+this.activateRoute.snapshot.paramMap.get('id'))
-      .subscribe(
-        (product) => {
-          this.product = product;
-          this.bcService.set('@productDetails', product.name);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(
+      (product) => {
+        this.product = product;
+        this.bcService.set('@productDetails', product.name);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
