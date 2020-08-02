22971777c35fd0a9d8cca2806d7f885a34f23045
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-02 18:58:35 +0700

    ok

--- client/src/app/basket/basket.module.ts
+++ client/src/app/basket/basket.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { BasketRoutingModule } from './basket-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,6 +6,6 @@ import { BasketComponent } from './basket.component';
 
 @NgModule({
   declarations: [BasketComponent],
-  imports: [CommonModule, BasketRoutingModule],
+  imports: [CommonModule, BasketRoutingModule, SharedModule],
 })
 export class BasketModule {}
