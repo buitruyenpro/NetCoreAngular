c3da9555a96b16c4f053763d9db85f59d03bf656
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-05 17:36:54 +0700

    ok

---
+++ Infrastructure/Services/OrderService.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Core.Specifications;
+
+namespace Infrastructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
