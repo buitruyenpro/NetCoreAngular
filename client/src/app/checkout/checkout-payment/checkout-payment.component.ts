4ef72ae79bf5b5eb795fb5af216c7106794b86b0
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-15 21:19:48 +0700

    ok

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -2,10 +2,10 @@ import {
   Component,
   OnInit,
   Input,
+  AfterViewInit,
   ViewChild,
   ElementRef,
   OnDestroy,
-  AfterViewInit,
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BasketService } from 'src/app/basket/basket.service';
@@ -16,6 +16,7 @@ import { IOrder } from 'src/app/shared/models/order';
 import { Router, NavigationExtras } from '@angular/router';
 
 declare var Stripe;
+
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
@@ -23,7 +24,6 @@ declare var Stripe;
 })
 export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   @Input() checkoutForm: FormGroup;
-  // Static là cho phép insert Element vào
   @ViewChild('cardNumber', { static: true }) cardNumberElement: ElementRef;
   @ViewChild('cardExpiry', { static: true }) cardExpiryElement: ElementRef;
   @ViewChild('cardCvc', { static: true }) cardCvcElement: ElementRef;
@@ -46,13 +46,10 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   ) {}
 
   ngAfterViewInit() {
-    this.stripe = Stripe(
-      'pk_test_51HGFNlHmkTuwwu2HSQqqMQ59iivrQvP0b5EQ5wtZEljoeYhV0DlpdG5ryzEMjY4byEPzj1eSTzxdFv0nYeRwgLKT00VTJU7JCl'
-    );
+    this.stripe = Stripe('pk_test_2PZ84pFKu2MddUgGDG521v9m00SlLWySIR');
     const elements = this.stripe.elements();
 
     this.cardNumber = elements.create('cardNumber');
-    // Mount có nghĩa là mình lấy phần tử carNumber inser vào cardNumberElement
     this.cardNumber.mount(this.cardNumberElement.nativeElement);
     this.cardNumber.addEventListener('change', this.cardHandler);
 
@@ -64,11 +61,13 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     this.cardCvc.mount(this.cardCvcElement.nativeElement);
     this.cardCvc.addEventListener('change', this.cardHandler);
   }
+
   ngOnDestroy() {
     this.cardNumber.destroy();
     this.cardExpiry.destroy();
     this.cardCvc.destroy();
   }
+
   onChange(event) {
     if (event.error) {
       this.cardErrors = event.error.message;
@@ -108,6 +107,7 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
       this.loading = false;
     }
   }
+
   private async confirmPaymentWithStripe(basket) {
     return this.stripe.confirmCardPayment(basket.clientSecret, {
       payment_method: {
