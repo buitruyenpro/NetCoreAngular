d5e73a0e41d2e8d5313298277d36da1262e281a4
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-15 11:59:49 +0700

    ok

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,6 +20,15 @@ export class BasketService {
   shipping = 0;
 
   constructor(private http: HttpClient) {}
+
+  createPaymentIntent() {
+    return this.http.post(this.baseUrl + 'payments/' + this.getCurrentBasketValue().id, {}).pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+      })
+    );
+  }
+
   setShippingPrice(deliveryMethod: IDeliveryMethod) {
     this.shipping = deliveryMethod.price;
     const basket = this.getCurrentBasketValue();
