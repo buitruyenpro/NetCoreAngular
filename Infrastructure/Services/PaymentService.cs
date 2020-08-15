bdaf922ae4552b99f93007030322783eac916a86
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-15 10:00:16 +0700

    ok

---
+++ Infrastructure/Services/PaymentService.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using Order = Core.Entities.OrderAggregate.Order;
+using Product = Core.Entities.Product;
+
+namespace Infrastructure.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> UpdateOrderPaymentFailed(string paymentIntentId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> UpdateOrderPaymentSucceeded(string paymentIntentId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
