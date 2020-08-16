81956f2952135c1e9d1889b572dd4156d62b3e3a
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-16 11:55:35 +0700

    ok

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -14,6 +14,7 @@ namespace API.Controllers
     public class PaymentsController : BaseApiController
     {
         private readonly IPaymentService _paymentService;
+        private const string WhSecret = "whsec_EyP6oX8cWkG8pdnUHrzSOWnkoAWRwbco";
         private readonly ILogger<IPaymentService> _logger;
         public PaymentsController(IPaymentService paymentService, ILogger<IPaymentService> logger)
         {
@@ -30,6 +31,35 @@ namespace API.Controllers
             if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
 
             return basket;
+        }
+
+        [HttpPost("webhook")]
+        public async Task<ActionResult> StripeWebhook()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+
+            PaymentIntent intent;
+            Order order;
+
+            switch (stripeEvent.Type)
+            {
+                case "payment_intent.succeeded":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Succeeded: ", intent.Id);
+                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+                    _logger.LogInformation("Order updated to payment received: ", order.Id);
+                    break;
+                case "payment_intent.payment_failed":
+                    intent = (PaymentIntent)stripeEvent.Data.Object;
+                    _logger.LogInformation("Payment Failed: ", intent.Id);
+                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+                    _logger.LogInformation("Payment Failed: ", order.Id);
+                    break;
+            }
+
+            return new EmptyResult();
         }
 
     }
