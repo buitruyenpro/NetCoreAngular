2aba8a5d553643f786a1813ac648db421be5fe14
buitruyenpro <namtruyenpro95@gmail.com>
2020-08-18 11:52:45 +0700

    ok

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,44 +1,59 @@
+using System.IO;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Infrastructure.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 using Microsoft.Extensions.FileProviders;
-using System.IO;
+using StackExchange.Redis;
 
 namespace API
 {
     public class Startup
     {
         private readonly IConfiguration _config;
-
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
 
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<StoreContext>(x =>
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<StoreContext>(x =>
+                x.UseMySql(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseMySql(_config.GetConnectionString("IdentityConnection"));
+            });
+
+            ConfigureServices(services);
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddDbContext<StoreContext>(x =>
-                            x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-
-            services.AddDbContext<AppIdentityDbContext>(x =>
-                     {
-                         x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
-                     });
 
             services.AddSingleton<IConnectionMultiplexer>(c =>
             {
@@ -47,7 +62,6 @@ namespace API
                 return ConnectionMultiplexer.Connect(configuration);
             });
 
-            // Đây là khi đầu vào input không hợp lệ
             services.AddApplicationServices();
             services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
@@ -58,15 +72,12 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
             });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // Đây là khi lỗi server
             app.UseMiddleware<ExceptionMiddleware>();
-            // Đây là trang không tìm thấy
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
@@ -80,10 +91,14 @@ namespace API
                 ),
                 RequestPath = "/content"
             });
+
             app.UseCors("CorsPolicy");
+
             app.UseAuthentication();
             app.UseAuthorization();
+
             app.UseSwaggerDocumention();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
